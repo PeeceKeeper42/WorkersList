847a7d9eee8bfed6b0e09b350e090546ce481d88
Churkin Vladimir <VC160420@mail.ru>
2022-07-09 01:27:02 +0300

    Revert "Revert "Finished implementing configuration files""
    This reverts commit cad9e50cab28b62fe39f163eaa5b33ebffd89bc9.

---
+++ WorkersList.DataAccessLayer/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WorkersList.DataAccessLayer.Entities;
+
+namespace WorkersList.DataAccessLayer.Configurations
+{
+    public class ReviewConfiguration : IEntityTypeConfiguration<Review>
+    {
+        public void Configure(EntityTypeBuilder<Review> entityTypeBuilder)
+        {
+            /*
+                Fields
+            */
+            entityTypeBuilder.Property(x => x.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            entityTypeBuilder.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(64)
+                .HasDefaultValue("Title");//move to restriction file
+
+            entityTypeBuilder.Property(x => x.Text)
+                .IsRequired()
+                .HasMaxLength(64)
+                .HasDefaultValue("Text");//move to restriction file
+
+            entityTypeBuilder.Property(x => x.Liked)
+                .IsRequired();
+
+            entityTypeBuilder.Property(x => x.Disliked)
+                .IsRequired();
+
+            entityTypeBuilder.Property(x => x.LastModifyInfoDate)
+                .IsRequired()
+                .HasDefaultValueSql("getdate()");//move to restriction file
+            /*
+                Connections
+            */
+            entityTypeBuilder
+                .HasMany(x => x.Comments)
+                .WithOne(x => x.Review)
+                .HasForeignKey(x => x.ReviewId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
--- WorkersList.DataAccessLayer/Entities/Comment.cs
+++ WorkersList.DataAccessLayer/Entities/Comment.cs
@@ -8,7 +8,9 @@
         }
         public int Id { get; set; }
         public string Text { get; set; }
-        public byte Rate { get; set; }
+        public int Liked { get; set; }
+        public int Disliked { get; set; }
+        public DateTimeOffset LastModifyInfoDate { get; set; }
 
         //One to Many Worker - Comments
         public Worker Worker { get; set; }
@@ -20,5 +22,7 @@
 
         //One to Many Comment - Comments
         public ICollection<Comment> Comments { get; set; }
+        public int? ParentCommentId { get; set; }
+        public Comment ParentComment { get; set; }
     }
 }

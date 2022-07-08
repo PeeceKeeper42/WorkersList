c79345d8bd8dccaef5efaee0851bc4aef3b47d02
Churkin Vladimir <VC160420@mail.ru>
2022-07-09 01:46:08 +0300

    Implemented MainContext
    ...Also implement configuration file for pass

---
+++ WorkersList.DataAccessLayer/Configurations/WorkerPasswordConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WorkersList.DataAccessLayer.Entities;
+
+namespace WorkersList.DataAccessLayer.Configurations
+{
+    public class WorkerPasswordConfiguration : IEntityTypeConfiguration<WorkerPassword>
+    {
+        public void Configure(EntityTypeBuilder<WorkerPassword> entityTypeBuilder)
+        {
+            /*
+                Fields
+            */
+            entityTypeBuilder.Property(x => x.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            entityTypeBuilder.Property(x => x.Salt)
+                .IsRequired();
+
+            entityTypeBuilder.Property(x => x.HashedPassword)
+                .IsRequired();
+            /*
+                Connections
+            */
+        }
+    }
+}
---
+++ WorkersList.DataAccessLayer/Contexts/MainContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkersList.DataAccessLayer.Entities;
+
+namespace WorkersList.DataAccessLayer.Contexts
+{
+    public class MainContext : DbContext
+    {
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<WorkerPassword> WorkerPasswords { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        public MainContext()
+        {
+            Database.EnsureCreated();
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=WIN-PJK3AQQHR2L;Initial Catalog = WorkersListDB;Integrated Security=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
